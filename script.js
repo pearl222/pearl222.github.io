a361637504acf75cf24eb62a4f74e7c649858f1f
Lilly Magic <pearl@MacBook-Air-di-Ivonna.local>
2022-11-14 15:11:00 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+//let darkMode = document.getElementById('dark-mode');
+//let bodyStyle = document.body.style.backgroundColor = 'white';
+//let colorStyle = document.body.style.color = 'black';
+
+
+
+
+function myToggle() {
+ let element = document.body;
+ element.classList.toggle('dark-mode'); 
+}
+
+
